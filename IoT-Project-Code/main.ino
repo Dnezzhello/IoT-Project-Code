c0e8ddf5e94f73a87ecf28d582c1c7c89e83d582
h3llohihi <71027676+Dnezzhello@users.noreply.github.com>
2024-07-07 15:12:18 +0700

    Update main.ino
    - regularly check soil moisture every 3 sec
    - the pump will start working until moisture reaches 70%
    - remove dht sensor part

--- IoT-Project-Code/main.ino
+++ IoT-Project-Code/main.ino
@@ -1,52 +1,57 @@
 #include <LiquidCrystal_I2C.h>
-#include "DHT.h"
 
 #define MOISTUREPIN A0
 #define RELAY 3
-#define DHTPIN 2
-#define DHTTYPE DHT11
-#define WETVALUE 0
-#define DRYVAlUE 1023
-#define MIN_MOISTURE_PERCENTAGE 40
+#define WETVALUE 389
+#define DRYVAlUE 989
+#define MIN_MOISTURE_PERCENTAGE 20
+#define TARGET_MOISTURE_PERCENTAGE 70
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
      // initialise the lcd
     lcd.init();
     lcd.backlight();
 
-    // initialise dht sensor
-    dht.begin();
-
-    // indicate that the pump is not working yet
-    digitalWrite(RELAY, LOW);
-
     // show welcoming message
     welcomMessage();
 }
 
 void loop() {
-    float airTemperature = readAirTemperature();
-    float airHumidity = readAirHumidity();
     float soilMoisture = readSoilMoisture();
 
-    showInfo(airTemperature, airHumidity, soilMoisture);
+    showInfo(soilMoisture);
 
     if (soilMoisture < MIN_MOISTURE_PERCENTAGE) {
         lcd.clear();
         lcd.setCursor(0,0);
-        lcd.print("DRY SOIL");
+        lcd.print("SOIL IS DRY");
         lcd.setCursor(0,1);
         lcd.print("PUMP WORKING");
-        
+
+        delay(1000);
+
         digitalWrite(RELAY, HIGH); // turn on the pump
-        delay(3000);
-        digitalWrite(RELAY, LOW); // turn off the pump
+
+        while (true) {
+            showInfo(soilMoisture);
+            soilMoisture = readSoilMoisture();
+
+            if (soilMoisture > TARGET_MOISTURE_PERCENTAGE) {
+                digitalWrite(RELAY, LOW); // turn off the pump
+
+                lcd.clear();
+                lcd.setCursor(0,0);
+                lcd.print("PUMP STOPPED");
+
+                break;
+            }
+            delay(250);
+        }
     }
 
-    delay(4000);
+    delay(3000);
 }
 
 void welcomMessage() {
@@ -67,43 +72,12 @@ void welcomMessage() {
     lcd.clear();
 }
 
-void showInfo(float t, float h, float m) {
+void showInfo(float m) {
     lcd.clear();
-    // first line: temperature and humidity read from dht sensor
-    String firstLine = "T:" + String(t, 1) + "C  H:" + String(h, 1) + "%";
+
     lcd.setCursor(0,0);
+    String firstLine = "Moist: " + String(m, 1) + "%";
     lcd.print(firstLine);
-    
-    // second line: soil moisture read from the resistive sensor
-    lcd.setCursor(0,1);
-    String secondLine = "M:" + String(m, 1) + "%";
-    lcd.print(secondLine);
-}
-
-float readAirTemperature() {
-    float t = dht.readTemperature();
-
-    if (isnan(t)) {
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("TEMP. FAILED");
-        return;
-    }
-
-    return t;
-}
-
-float readAirHumidity() {
-    float h = dht.readHumidity();
-
-    if (isnan(h)) {
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("HUMID. FAILED");
-        return;
-    }
-
-    return h;
 }
 
 float readSoilMoisture() {
